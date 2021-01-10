7429acf2f5bf97ba59376549dd6d74fa7091cad4
Andrew Huntington <andrew.huntington@gmail.com>
2021-01-10 19:50:26 +0900

    solve ex 2-3

--- ch_2/exercises/2-3.cpp
+++ ch_2/exercises/2-3.cpp
@@ -14,14 +14,37 @@
 using std::cout;
 using std::cin;
 
-int main() {
+int main() 
+{
   // Top half
-  for(int row = 1; row <= 4; row++) {
-    for(int col = 1; col <= 14; col++){
-      if ((col >= row && col < row * 2) || (col <= 14 - row + 1 && col >= 14 - row * 2 + 2)){
+  for(int row = 1; row <= 4; row++) 
+  {
+    for(int col = 1; col <= 14; col++)
+    {
+      if ((col >= row && col < row * 2) ||
+          (col <= 14 - row + 1 && col >= 14 - row * 2 + 2))
+      {
         cout << "#";
-      } else {
-        cout << "-";
+      } else 
+      {
+        cout << " ";
+      }
+    }
+    cout << "\n";
+  }
+
+  // Bottom half
+  for(int row = 4; row >= 1; row--) 
+  {
+    for(int col = 1; col <= 14; col++)
+    {
+      if ((col >= row && col < row * 2) ||
+          (col <= 14 - row + 1 && col >= 14 - row * 2 + 2))
+      {
+        cout << "#";
+      } else 
+      {
+        cout << " ";
       }
     }
     cout << "\n";
