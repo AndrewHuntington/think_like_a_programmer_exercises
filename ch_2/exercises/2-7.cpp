9d4f5e6bf18eac7f1566e2777425ca596ce09e18
Andrew Huntington <andrew.huntington@gmail.com>
2021-01-16 15:28:41 +0900

    start exercise 2-7

---
+++ ch_2/exercises/2-7.cpp
@@ -0,0 +1,15 @@
+// Exercise 2-7
+// Convert between binary, decimal, and hexidecimal numbers
+
+// TODO: Create menu
+// TODO: handle decimal <-> hexadecimal (0-9 A-F) conversions
+// TODO: handle binary <-> hex conversions
+// TODO: Move code from 2-6 to handle binary <-> decimal
+
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main() {
+
+}
