7a3532b801f06fe53ecfd626f0cdc1eff2ea5cf3
Andrew Huntington <andrew.huntington@gmail.com>
2021-01-21 20:19:26 +0900

    rename files, add 2-8 and 2-9

---
+++ ch_2/exercises/2-8_base16_or_less_convert.cpp
@@ -0,0 +1,12 @@
+// Exercise 2-8
+// Convert between any numbers, base16 or lower
+
+// May come back to this, as I'm tired of number convertions...
+
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main() {
+  return 0;
+}
---
+++ ch_2/exercises/2-9_text_stats.cpp
@@ -0,0 +1,16 @@
+// Exercise 2-9
+// A program that reads a line of text and outputs various information about it
+
+// Features to implement:
+// count the number of words
+// indentify the length of the longest word
+// the greatest number of vowels in a word
+// extra...
+
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main() {
+  return 0;
+}
